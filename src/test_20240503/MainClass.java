f034bf8aca12a3fe9bedb0db98b7794632ff7940
shth55555 <shth5555@naver.com>
2024-05-03 10:11:23 +0900

    helloworld

---
+++ src/test_20240503/MainClass.java
@@ -0,0 +1,12 @@
+package test_20240503;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+	}
+
+}
